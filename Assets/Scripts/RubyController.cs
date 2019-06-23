3d0cd6bbca3f2f5539d1c560291a0842a91b4aa2
raul_cuth <raul.cuth@gmail.com>
2019-06-23 12:26:34 +0300

    extracted tiles, added prefabs, added starting environment

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -8,8 +8,8 @@ public class RubyController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal * Time.deltaTime;
-        position.y = position.y + 0.1f * vertical * Time.deltaTime;
+        position.x = position.x + 0.1f * horizontal;// * Time.deltaTime;
+        position.y = position.y + 0.1f * vertical;// * Time.deltaTime;
         transform.position = position;
     }
 }
