d07af00abf1b81c74dfc3ca0c538cda1ccc7edcb
raul_cuth <raul.cuth@gmail.com>
2019-06-23 13:30:15 +0300

    added colliders and composit tile colliders

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -1,15 +1,23 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    private Rigidbody2D _rigidbody2D;
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;// * Time.deltaTime;
-        position.y = position.y + 0.1f * vertical;// * Time.deltaTime;
-        transform.position = position;
+        Vector2 position = _rigidbody2D.position;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        _rigidbody2D.MovePosition(position);
     }
 }
