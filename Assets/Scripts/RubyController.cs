fe1343bc5555795068ac72263f01fffcb899874e
raul_cuth <raul.cuth@gmail.com>
2019-06-23 09:23:48 +0300

    added basic movement and main script

---
+++ Assets/Scripts/RubyController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 position = transform.position;
+        position.x = position.x + 0.1f * horizontal * Time.deltaTime;
+        position.y = position.y + 0.1f * vertical * Time.deltaTime;
+        transform.position = position;
+    }
+}
